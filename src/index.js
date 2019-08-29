7098a2c92ac4bb45b3c7335df6af7985b27a6e17
Quintin Soto <quintin.soto96@gmail.com>
2019-08-29 15:29:11 -0400

    react  config

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import App from "./App";
+import style from "./main.css";
+console.log('Soy un punto de entrada');
+const arr = [1, 2, 3];
+const iAmJavascriptES6 = () => console.log(...arr);
+window.iAmJavascriptES6 = iAmJavascriptES6;
